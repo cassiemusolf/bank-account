6809abbb2b8e1d58f309340de2d9a41341648f8e
Cassie Musolf and Kory Skarbek <cassie@>
2017-01-24 16:23:56 -0800

    NOW it is done

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,58 @@
+var startAccount;
 //business logic
 function BankAccount(registerName, initialDeposit) {
   this.userName = registerName;
   this.money = initialDeposit;
 }
 
-function ChangeAccount(depositAmount, withdrawalAmount, money) {
-  this.deposit = depositAmount;
-  this.withdrawal = withdrawalAmount;
-  this.newBalance  = money
+BankAccount.prototype.depositMoney = function(depositAmount) {
+  this.money += depositAmount;
 }
 
-ChangeAccount.prototype.accountBalance = function () {
-  if (this.deposit > 0){
-    this.newBalance += this.deposit;
-  }
-  if (this.withdrawal > 0){
-    this.newBalance -= this.withdrawal;
-  }
-  return this.newBalance;
-};
+BankAccount.prototype.withdrawMoney = function(withdrawAmount) {
+  this.money -= withdrawAmount;
+}
 
-
-
+// function ChangeAccount(depositAmount, withdrawalAmount, money) {
+//   this.deposit = depositAmount;
+//   this.withdrawal = withdrawalAmount;
+//   this.newBalance  = money;
+// }
+//
+// ChangeAccount.prototype.accountBalance = function () {
+//   if (this.deposit > 0){
+//     this.newBalance += this.deposit;
+//   }
+//   if (this.withdrawal > 0){
+//     this.newBalance -= this.withdrawal;
+//   }
+//   return this.newBalance;
+// };
 
 //user interface logic
 $(document).ready(function() {
   var initialDeposit;
+
   $("#register").submit(function() {
     event.preventDefault();
-
     var registerName = $('#user-name').val();
     initialDeposit = parseInt($('#first-deposit').val());
-
-    var startAccount = new BankAccount(registerName, initialDeposit);
-
+    startAccount = new BankAccount(registerName, initialDeposit);
     $('.account-balance').text('$' + startAccount.money);
+    $('#user-name').val("");
+    $('#first-deposit').val("");
   });
 
   $("#transactions").submit(function() {
     event.preventDefault();
-
     var depositAmount = parseInt($('#deposit-amount').val());
     var withdrawalAmount = parseInt($('#withdrawal-amount').val());
-
-    var transaction = new ChangeAccount(depositAmount, withdrawalAmount, initialDeposit);
-
-    $('.account-balance').text('$' + transaction.accountBalance());
-
+    if (depositAmount > 0){
+      startAccount.depositMoney(depositAmount);
+    }
+    if (withdrawalAmount > 0) {
+      startAccount.withdrawMoney(withdrawalAmount);
+    }
+    $('.account-balance').text('$' + startAccount.money);
   });
 });
